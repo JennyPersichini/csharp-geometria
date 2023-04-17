cdf4164ef4c10e5319cbad1dccb41cc01bb66f2b
JennyPersichini <nenny1096@gmail.com>
2023-04-17 14:43:10 +0200

    Aggiungere i file di progetto.

---
+++ csharp-geometria/Rettangolo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Creare una classe Rettangolo con due attributi interi: baseRettangolo e altezzaRettangolo.
+//Aggiungere un opportuno costruttore con parametri.
+//Aggiungere due metodi: calcolaArea e calcolaPerimetro che calcolano e restituiscono, rispettivamente, l’area e il perimetro del rettangolo.
+
+namespace csharp_geometria
+{
+    internal class Rettangolo
+    {
+    }
+}
