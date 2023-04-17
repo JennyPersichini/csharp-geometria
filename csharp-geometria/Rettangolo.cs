e8e49b6bf87b9481f8f0283f78ff5c3e754ab693
JennyPersichini <nenny1096@gmail.com>
2023-04-17 16:07:25 +0200

    Init bonus

--- csharp-geometria/Rettangolo.cs
+++ csharp-geometria/Rettangolo.cs
@@ -43,6 +43,16 @@ namespace csharp_geometria
             Console.WriteLine($"area: {calcolaArea()} cm2");
         }
 
+        //BONUS:
+        //Aggiungere alla classe Rettangolo un metodo “disegna” che disegna in console il rettangolo con le sue dimensioni,
+        //ossia tanti “—” (due trattini) orizzontali quanto è grande la sua base 
+        //e tanti ‘|’ verticali quanto e grande la sua altezza.
+
+        public void DisegnaRettangolo()
+        {
+
+        }
+
     }
 
 }
